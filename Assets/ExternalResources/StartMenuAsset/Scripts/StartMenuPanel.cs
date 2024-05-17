3fbecfa8fcee34f7295718329fce8e06987f6bb9
wolfheat <mihe82@gmail.com>
2024-05-18 01:21:49 +0200

    refactor Start Menu

--- Assets/ExternalResources/StartMenuAsset/Scripts/StartMenuPanel.cs
+++ Assets/ExternalResources/StartMenuAsset/Scripts/StartMenuPanel.cs
@@ -3,21 +3,12 @@ using UnityEngine;
 namespace Wolfheat.StartMenu
 {
     public class StartMenuPanel : MonoBehaviour
-{
-    [SerializeField] GameObject panel;
-    public Animator animator;
+    {
+        [SerializeField] GameObject panel;
+        public Animator animator;
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
+        private void Awake() => animator = GetComponent<Animator>();
+        public void Disable() => panel.SetActive(false);
+        public void AnimationComplete() => StartMenuController.Instance.AnimationComplete();
     }
-    public void Disable()
-    {
-        panel.SetActive(false);
-    }
-    public void AnimationComplete()
-    {
-        StartMenuController.Instance.AnimationComplete();
-    }
-}
 }
